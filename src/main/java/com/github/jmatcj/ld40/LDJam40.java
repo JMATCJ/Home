52f6874cbdb1933060328c4805076dc0ec906aa9
Parker Young <parker@parker8283.net>
2017-12-01 22:23:00 -0500

    Testing drawing

--- src/main/java/com/github/jmatcj/ld40/LDJam40.java
+++ src/main/java/com/github/jmatcj/ld40/LDJam40.java
@@ -1,9 +1,11 @@
 package com.github.jmatcj.ld40;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class LDJam40 extends Application {
@@ -15,9 +17,17 @@ public class LDJam40 extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas();
+        Canvas canvas = new Canvas(200, 200);
         Scene scene = new Scene(new Group(canvas));
         primaryStage.setScene(scene);
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                canvas.getGraphicsContext2D().setFill(Color.RED);
+                canvas.getGraphicsContext2D().fillRect(10, 10, 10, 10);
+            }
+        }.start();
         primaryStage.show();
     }
 
