aa83abac6ad6aaf3665c19a2fb550aa1b87eba73
Parker Young <parker@parker8283.net>
2017-12-01 22:39:30 -0500

    Added Resources Enum

---
+++ src/main/java/com/github/jmatcj/ld40/Resources.java
@@ -0,0 +1,14 @@
+package com.github.jmatcj.ld40;
+
+public enum Resources {
+    FOOD,
+    STONE,
+    IRON,
+    CARBON,
+    COPPER,
+    SILICON,
+    STEEL,
+    HEXAPHESTRITE,
+    ISOBELGOL,
+    SELDROLE
+}
