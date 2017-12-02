f284bfd2464c1f1fb9a8e53f628a03e56642e7bb
Trevor <tdgood98@gmail.com>
2017-12-01 22:17:13 -0500

    Making the (blank) window pop up

--- src/main/java/com/github/jmatcj/ld40/LDJam40.java
+++ src/main/java/com/github/jmatcj/ld40/LDJam40.java
@@ -1,6 +1,9 @@
 package com.github.jmatcj.ld40;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 
 public class LDJam40 extends Application {
@@ -12,7 +15,10 @@ public class LDJam40 extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Canvas canvas = new Canvas();
+        Scene scene = new Scene(new Group(canvas));
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     @Override
@@ -21,6 +27,6 @@ public class LDJam40 extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch(LDJam40.class, args);
     }
 }
