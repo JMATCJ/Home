be235be9a652fd289726ac530ff85438781ff30a
Parker Young <parker@parker8283.net>
2017-12-03 14:36:32 -0500

    Added Upgrade Enum

---
+++ src/main/java/com/github/jmatcj/ld40/data/Upgrade.java
@@ -0,0 +1,25 @@
+package com.github.jmatcj.ld40.data;
+
+public enum Upgrade {
+    HARVESTER(Resource.FOOD, 50);
+
+    private Resource resNeeded;
+    private int amtNeeded;
+
+    Upgrade(Resource resNeeded, int amtNeeded) {
+        this.resNeeded = resNeeded;
+        this.amtNeeded = amtNeeded;
+    }
+
+    public boolean canUnlock(Resource r, int amtOnHand) {
+        return r == resNeeded && amtOnHand >= amtNeeded;
+    }
+
+    public Resource getResourceNeeded() {
+        return resNeeded;
+    }
+
+    public int getAmountNeeded() {
+        return amtNeeded;
+    }
+}
