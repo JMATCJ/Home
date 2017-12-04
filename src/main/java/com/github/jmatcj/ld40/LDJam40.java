02466ddaed607c96902994143a0462c376ca2948
Parker Young <parker@parker8283.net>
2017-12-04 08:39:06 -0500

    Potentially fixed slow performance when run from a JAR on some platforms.

--- src/main/java/com/github/jmatcj/ld40/LDJam40.java
+++ src/main/java/com/github/jmatcj/ld40/LDJam40.java
@@ -2,19 +2,16 @@ package com.github.jmatcj.ld40;
 
 import com.github.jmatcj.ld40.data.Planet;
 import com.github.jmatcj.ld40.util.AssetLoader;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class LDJam40 extends Application {
-    private Timeline gameLoop;
+    private AnimationTimer gameLoop;
     private Game game;
 
     @Override
@@ -32,24 +29,21 @@ public class LDJam40 extends Application {
         Canvas canvas = new Canvas(1280, 720);
         Scene scene = new Scene(new Group(canvas));
         primaryStage.setScene(scene);
-
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         scene.setOnMouseClicked(event -> game.onClick(event));
 
-        gameLoop = new Timeline();
-        gameLoop.setCycleCount(Timeline.INDEFINITE);
-        KeyFrame loop = new KeyFrame(Duration.millis(16.67), event -> { // ~60FPS
-            long now = System.nanoTime();
-            game.update(now);
+        gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                game.update(now);
 
-            gc.clearRect(0, 0, 1280, 720);
-            gc.drawImage(game.getCurrentPlanet().getBackground(), 0, 0);
-            game.draw(gc);
-        });
-
-        gameLoop.getKeyFrames().add(loop);
-        gameLoop.play();
+                gc.clearRect(0, 0, 1280, 720);
+                gc.drawImage(game.getCurrentPlanet().getBackground(), 0, 0);
+                game.draw(gc);
+            }
+        };
+        gameLoop.start();
 
         primaryStage.show();
 
